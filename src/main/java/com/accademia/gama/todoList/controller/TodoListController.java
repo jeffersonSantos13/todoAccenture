7279be4bae5989caa2919e893a98dda65ae34481
Jefferson Santos <yuiti.santos@gmail.com>
2020-10-24 17:45:23 -0300

    [add: new Features, delete, put, get description, get todo complete or not]

--- src/main/java/com/accademia/gama/todoList/controller/TodoListController.java
+++ src/main/java/com/accademia/gama/todoList/controller/TodoListController.java
@@ -3,6 +3,7 @@ package com.accademia.gama.todoList.controller;
 import com.accademia.gama.todoList.entity.TodoList;
 import com.accademia.gama.todoList.service.TodoListService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -31,6 +32,32 @@ public class TodoListController {
     @GetMapping("/{todoListId}")
     public ResponseEntity<TodoList> getTodoListById(@PathVariable int todoListId) {
         return ResponseEntity.ok(service.getTodoListById(todoListId));
+    }
+
+    @PutMapping("/{todoListId}")
+    public ResponseEntity<TodoList> updateTodoList(@PathVariable int todoListId, @RequestBody TodoList todoList) {
+        return ResponseEntity.ok(service.putTodoList(todoListId, todoList));
+    }
+
+    @GetMapping("/todoListComplete")
+    public ResponseEntity<List<TodoList>> getAllTodoListCompleted() {
+        return ResponseEntity.ok(service.getAllTodoListCompleted());
+    }
+
+    @GetMapping("/todoListIsNotComplete")
+    public ResponseEntity<List<TodoList>> getAllTodoListIsNotCompleted() {
+        return ResponseEntity.ok(service.getAllTodoListIsNotCompleted());
+    }
+
+    @GetMapping("/{todoListId}/description")
+    public ResponseEntity<Object> updateTodoList(@PathVariable int todoListId) {
+        return ResponseEntity.ok(service.getTodoListByIdDescription(todoListId));
+    }
+
+    @DeleteMapping("/{todoListId}")
+    public ResponseEntity<String> deleteTodoList(@PathVariable int todoListId) {
+        service.deleteTodoList(todoListId);
+        return new ResponseEntity<>("", HttpStatus.OK);
     }
 
 }
--- src/main/java/com/accademia/gama/todoList/service/TodoListService.java
+++ src/main/java/com/accademia/gama/todoList/service/TodoListService.java
@@ -3,7 +3,7 @@ package com.accademia.gama.todoList.service;
 import com.accademia.gama.todoList.entity.TodoList;
 import com.accademia.gama.todoList.repository.TodoListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,7 +15,7 @@ public class TodoListService {
     private TodoListRepository repository;
 
     public TodoList saveTodoList(TodoList todoList) {
-        return repository.save(todoList);
+        return repository. save(todoList);
     }
 
     public List<TodoList> getAllTodoList() {
@@ -25,5 +25,37 @@ public class TodoListService {
     public TodoList getTodoListById(int todoListId) {
         return repository.findById(todoListId)
                 .orElseThrow();
+    }
+
+    public TodoList putTodoList(int todoListId, TodoList todoList) {
+        TodoList todoListUpdated = repository.findById(todoListId).orElseThrow();
+        todoListUpdated.setDescription(todoList.getDescription());
+        todoListUpdated.setComplete(todoList.isComplete());
+        todoListUpdated.setTitle(todoList.getTitle());
+        repository.save(todoListUpdated);
+        return todoListUpdated;
+    }
+
+    public List<TodoList> getAllTodoListCompleted() {
+        List<TodoList> listsTodoList = (List<TodoList>) repository.findAll();
+        listsTodoList.removeIf(list -> !list.isComplete());
+        return listsTodoList;
+    }
+
+    public List<TodoList> getAllTodoListIsNotCompleted() {
+        List<TodoList> listsTodoList = (List<TodoList>) repository.findAll();
+        listsTodoList.removeIf(TodoList::isComplete);
+        return listsTodoList;
+    }
+
+    public Object getTodoListByIdDescription(int todoListId) {
+        TodoList todoList = repository.findById(todoListId).orElse(new TodoList());
+
+        return todoList.getDescription();
+    }
+
+    public void deleteTodoList(int todoListId) {
+        TodoList deleteTodoList = repository.findById(todoListId).orElseThrow();
+        repository.delete(deleteTodoList);
     }
 }
